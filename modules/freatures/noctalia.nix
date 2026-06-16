33e317a034b5d5b87d23f1319f729a91a1fcd87d
FilipiSantoz <239681201+FilipiSantoz@users.noreply.github.com>
2026-06-15 23:44:09 -0300

    Flake.lock update

--- modules/freatures/noctalia.nix
+++ modules/freatures/noctalia.nix
@@ -9,6 +9,7 @@
         systemd.enable = true;
 
         settings = {
+          launch_apps_as_systemd_services = true;
           theme = {
             mode = "dark";
             source = "builtin";
